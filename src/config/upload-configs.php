4fefa051f8b89aeb46ff4012286f6716bcfc58e8
Irineu Junior <irineu@mixd.com.br>
2021-01-22 15:09:32 -0300

    feat(articles) deleta somente as categorias do pacote e desabilita a imagem por padrão

--- src/config/upload-configs.php
+++ src/config/upload-configs.php
@@ -23,24 +23,24 @@ return [
         ],
     ],
     'articles-categories' => [
-//        'image' => [ //nome do campo
-//            'label' => 'imagem', //label do campo
-//            'multiple' => false, //se permite o upload multiplo
-//            'faker_dir' => false, #database_path('faker/articles-categories/image'),
-//            'sources' => [
-//                [
-//                    'conversion' => 'min-width-1366',
-//                    'media' => '(min-width: 1366px)',
-//                    'width' => 1024, // 16:9
-//                    'height' => 576,
-//                ],
-//                [
-//                    'conversion' => 'min-width-1280',
-//                    'media' => '(min-width: 1280px)',
-//                    'width' => 776,
-//                    'height' => 437,
-//                ],
-//            ],
-//        ],
+        'image' => [ //nome do campo
+            'label' => 'imagem', //label do campo
+            'multiple' => false, //se permite o upload multiplo
+            'faker_dir' => false, #database_path('faker/articles-categories/image'),
+            'sources' => [
+                [
+                    'conversion' => 'min-width-1366',
+                    'media' => '(min-width: 1366px)',
+                    'width' => 1024, // 16:9
+                    'height' => 576,
+                ],
+                [
+                    'conversion' => 'min-width-1280',
+                    'media' => '(min-width: 1280px)',
+                    'width' => 776,
+                    'height' => 437,
+                ],
+            ],
+        ],
     ],
 ];
