5e3b5507d5c21f2dbddb63115369998a0da9f3a7
Irineu Junior <irineu@mixd.com.br>
2021-01-22 17:38:05 -0300

    feat(articles) estende o request

--- src/Http/Requests/CategoryRequest.php
+++ src/Http/Requests/CategoryRequest.php
@@ -2,47 +2,9 @@
 
 namespace Agenciafmd\Articles\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
+use Agenciafmd\Categories\Http\Requests\CategoryRequest as BaseCategoryRequest;
 
-class CategoryRequest extends FormRequest
+class CategoryRequest extends BaseCategoryRequest
 {
-    protected $errorBag = 'admix';
-
-    public function rules()
-    {
-        return [
-            'is_active' => [
-                'required',
-                'boolean',
-            ],
-            'name' => [
-                'required',
-                'max:150',
-            ],
-            'description' => [
-                'nullable',
-            ],
-            'sort' => [
-                'nullable',
-            ],
-            'media' => [
-                'array',
-                'nullable',
-            ],
-        ];
-    }
-
-    public function attributes()
-    {
-        return [
-            'is_active' => 'ativo',
-            'name' => 'nome',
-            'description' => 'descrição',
-        ];
-    }
-
-    public function authorize()
-    {
-        return true;
-    }
+    //
 }
