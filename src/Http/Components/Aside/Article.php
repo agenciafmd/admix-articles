5b4d91c13d9a75d2f2ad88f0d61dfe62f766b5f0
Carlos Tamagawa <carlos@mixd.com.br>
2025-04-23 08:41:09 -0300

    fix(menu) ajusta o visible do menu

--- src/Http/Components/Aside/Article.php
+++ src/Http/Components/Aside/Article.php
@@ -4,6 +4,8 @@ namespace Agenciafmd\Articles\Http\Components\Aside;
 
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
+use Illuminate\Support\Facades\Gate;
+use Agenciafmd\Articles\Models\Article as ArticleModel;
 
 class Article extends Component
 {
@@ -21,7 +23,7 @@ class Article extends Component
         $this->label = __(config('admix-articles.name'));
         $this->url = route('admix.articles.index');
         $this->active = request()?->currentRouteNameStartsWith('admix.articles');
-        $this->visible = true;
+        $this->visible = Gate::allows('view', ArticleModel::class);
 
         return view('admix::components.aside.item');
     }
